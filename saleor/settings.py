d9c0dc14a0a959931f4f3cef1c42a67d68d34a8f
Ali <53838025+smjfaraazali@users.noreply.github.com>
2023-02-26 01:17:50 -0400

    Update settings.py

--- saleor/settings.py
+++ saleor/settings.py
@@ -47,8 +47,10 @@ def get_bool_from_env(name, default_value):
 
 DEBUG = get_bool_from_env("DEBUG", True)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'saleor-production.up.railway.app']
+ALLOWED_HOSTS = "saleor-production.up.railway.app"
 ALLOWED_CLIENT_HOSTS = "saleor-production.up.railway.app"
+
+SECRET_KEY="secret"
 
 SITE_ID = 1
 
